10ead65caf9a7d085a47a9716d88a9b46db7383e
Sheshasai <trashdude177@gmail.com>
2026-07-07 20:12:13 +0530

    Implement complete notes REST API

--- app/main.py
+++ app/main.py
@@ -4,8 +4,13 @@ from app.database import engine
 from app.models import Base
 from app.database import SessionLocal
 from app.models import Note
+from fastapi import HTTPException
 
-app = FastAPI()
+app = FastAPI(
+    title="WhatsUp Brain API",
+    description="A tiny REST API for storing notes",
+    version="1.0.0"
+)
 Base.metadata.create_all(bind=engine)
 
 @app.post("/notes")
@@ -36,3 +41,41 @@ def read_notes():
         }
         for note in notes
     ]
+
+@app.get("/notes/{id}")
+def read_note(id: int):
+    db = SessionLocal()
+
+    note = db.query(Note).filter(Note.id == id).first()
+    db.close()
+
+    if note is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Note not found brotherr!"
+        )
+    
+    return {
+        "id": note.id,
+        "note": note.content
+    }
+
+@app.delete("/notes/{id}")
+def delete_note(id: int):
+    db = SessionLocal()
+
+    note = db.query(Note).filter(Note.id == id).first()
+
+    if note is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Note to be deleted isn't found brooo!"
+        )
+    
+    db.delete(note)
+    db.commit()
+    db.close()
+
+    return {
+        "message": "Note deleted successfully."
+    }
